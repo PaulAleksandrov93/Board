05221ae65eac229f520603c42bafcbfdde9f8545
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-03-07 23:38:38 +0300

    display list of ads

--- bboard/views.py
+++ bboard/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse# Создаем экземпляр класса HttpResponse
+from .models import Bb
 
 def index(request):#request - экземпляр класса HttpRequest
-    return HttpResponse("Здесь будет выведен список объявлений.")#HttpResponse - отправляет строку в ответ на запрос
+    s = 'Список объявлений \r\n\r\n\r\n'
+    for bb in Bb.objects.order_by('-published'):
+        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')#HttpResponse - отправляет строку в ответ на запрос-
