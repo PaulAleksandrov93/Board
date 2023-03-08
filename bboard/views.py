e0836308f80d3b78b770f3622d9d03e2fb8ac5a5
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-03-08 11:56:27 +0300

    creat first template

--- bboard/views.py
+++ bboard/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse# Создаем экземпляр класса HttpResponse
+from django.template import loader
+
 from .models import Bb
 
 def index(request):#request - экземпляр класса HttpRequest
-    s = 'Список объявлений \r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('-published'):
-        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf-8')#HttpResponse - отправляет строку в ответ на запрос-
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs}
+    return HttpResponse(template.render(context, request))#HttpResponse - отправляет строку в ответ на запрос-
