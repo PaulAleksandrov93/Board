3c36ebd6716c4dc0887a23b7ce30bdae46a94482
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-03-09 22:18:30 +0300

    create superuser and rusification app

--- bboard/views.py
+++ bboard/views.py
@@ -1,10 +1,7 @@
-from django.http import HttpResponse# Создаем экземпляр класса HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Bb
 
 def index(request):#request - экземпляр класса HttpRequest
-    template = loader.get_template('bboard/index.html')
     bbs = Bb.objects.order_by('-published')
-    context = {'bbs': bbs}
-    return HttpResponse(template.render(context, request))#HttpResponse - отправляет строку в ответ на запрос-
+    return render (request, 'bboard/index.html', {'bbs': bbs})#HttpResponse - отправляет строку в ответ на запрос-
