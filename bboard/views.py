15d5e29837a23cecdeae28dbbaa5fb8d15d37d12
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-03-11 23:17:00 +0300

    set parameters for models and for field models

--- bboard/views.py
+++ bboard/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from .models import Bb
 
 def index(request):#request - экземпляр класса HttpRequest
-    bbs = Bb.objects.order_by('-published')
+    bbs = Bb.objects.all
     return render (request, 'bboard/index.html', {'bbs': bbs})#HttpResponse - отправляет строку в ответ на запрос-
