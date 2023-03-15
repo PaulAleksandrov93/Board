aa3e27fa839b28518cfbcdf2182d4b89b1132aeb
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-03-15 21:34:34 +0300

    create class-conroller BbCreateView

--- bboard/views.py
+++ bboard/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .forms import BbForm
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = '/bboard/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
 
 from .models import Bb
 from .models import Rubric
